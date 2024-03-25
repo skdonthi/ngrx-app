00d1034a53b997186cff5241143e1a23a85c9d22
Shiva Krishna D <shiva@xcontrol.de>
2024-03-25 11:57:54 +0100

    Define reducers

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -3,12 +3,28 @@ import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
   MetaReducer,
+  on,
 } from "@ngrx/store";
+import { AuthActions } from "../action-types";
 
 export const authFeatureKey = "auth";
 
-export interface State {}
+export interface AuthState {}
 
-export const reducers: ActionReducerMap<State> = {};
+export const initialAuthState: AuthState = {
+  user: undefined,
+};
+
+export const reducers: ActionReducerMap<AuthState> = {};
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user,
+    };
+  })
+);
